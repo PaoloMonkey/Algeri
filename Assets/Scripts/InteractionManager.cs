a46375dc1a0d558c5cd653906834d80b173d36c9
PaoloMonkey <paolo.taje@gmail.com>
2017-01-21 12:32:26 +0100

    added ignore folder in ignore

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private Camera mainCamera;
+    private Vector2 screenSize;
+    private Vector2 fixedPos;
+
+    public Vector2 cameraPan = Vector2.zero;
+
+    private void Awake()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        screenSize = new Vector2(Screen.width, Screen.height);
+        fixedPos = mainCamera.transform.position;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        Vector2 mousePosition = Input.mousePosition;
+
+        // change camera pos/rotation
+        mainCamera.transform.position
 	}
 }
