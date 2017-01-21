cd252505d56e3c34072a82963911cdc103568e9a
Pietro <mellowac@gmail.com>
2017-01-21 14:23:45 +0100

    Test rendering scene

---
+++ Assets/Scripts/cameaRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameaRotate : MonoBehaviour {
+
+	public Transform pivot;
+
+	public float smooth = 2.0F;
+	public float tiltAngle = 30.0F;
+
+
+	public Quaternion target;
+	void Update() {
+		
+		if (Random.value > 0.99) {
+			target = Quaternion.Euler(Random.Range(0,30), Random.Range(-180,180), Random.Range(-10,10));
+		}
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+	}
+}
