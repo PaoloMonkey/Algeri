cf2aa806c758b52ea4574d4444f33ac761b0b3b5
Pietro <mellowac@gmail.com>
2017-01-22 14:22:58 +0100

    exit enter credits

---
+++ Assets/Scripts/exitGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class exitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public bool loading = false;
+	// Update is called once per frame
+	void Update () {
+
+		if (loading == false && Input.GetKey (KeyCode.Escape)) {
+			SceneManager.LoadSceneAsync (0);
+			loading = true;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/loadGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class loadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public bool loading = false;
+	// Update is called once per frame
+	void Update () {
+
+		if (loading == false && (Input.GetMouseButtonDown (0) || Input.GetKey (KeyCode.Return))) {
+			SceneManager.LoadSceneAsync (1);
+			loading = true;
+		}
+
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.Quit();
+		}
+		
+	}
+}
